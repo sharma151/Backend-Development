3338829c0dd58c2bd5285192ade1fb966a0776af
sharma151 <sharmasaurav150503@gmail.com>
2025-12-04 13:29:09 +0545

    lear: middlware concept example added

--- Express Server/index.js
+++ Express Server/index.js
@@ -2,6 +2,18 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello World from Express Server");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Abour page");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Contact page");
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
